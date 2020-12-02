781f76675d879f93084f3a98d1bfee9a93fd4179
dmr <rajan@bottle.com.np>
2020-12-02 10:13:22 +0545

    adds creds for discord

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const express = require('express');
-const app = express();
-const port = process.env.port || 3000;
+// const express = require('express');
+// const app = express();
+// const port = process.env.port || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+let botToken =
+  process.env.DISCORD_BOT_TOKEN ||
+  'NzUyMTczMDIxMzQyNzI4MzQy.X1Txtg.LPggN14fdrMTbUBNHLnIMXrTA9U';
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+let userInput;
+
+client.once('ready', () => {
+  console.log('Discord Bot ready');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+client.on('message', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === 'Hello') {
+    message.channel.send('Hey there');
+  }
 });
+
+client.login(botToken);
