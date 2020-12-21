ff6c137078bc9c84be6d9d0566375261e733a352
dmr <rajan@bottle.com.np>
2020-12-21 21:34:49 +0545

    changes command type

--- index.js
+++ index.js
@@ -3,18 +3,29 @@ let botToken = process.env.DISCORD_BOT_TOKEN;
 const { Client } = require('discord.js');
 const client = new Client();
 
-let userInput;
+const prefix = '!'
 
 client.once('ready', () => {
   console.log('Discord Bot ready');
 });
 
-client.on('message', (message) => {
-  if (message.author.bot) return;
+client.on('message', async (message) => {
 
-  if (message.content === 'Hello') {
-    message.channel.send('Hey there');
+  try {
+    if(!message.content.startsWith(prefix) || (message.author.bot)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLocaleLowerCase();
+
+    await message.channel.send(command)
+  } catch (error) {
+    console.log(error);
   }
+  // if (message.author.bot) return;
+  //
+  // if (message.content === 'Hello') {
+  //   message.channel.send('Hey there');
+  // }
 });
 
 client.login(botToken);
