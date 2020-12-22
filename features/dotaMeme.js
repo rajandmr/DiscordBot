a6b69adf938e6a7b7be0a7b75ab12a9de42cd98b
Restyled.io <commits@restyled.io>
2020-12-22 05:27:40 +0000

    Restyled by clang-format

--- features/dotaMeme.js
+++ features/dotaMeme.js
@@ -1,15 +1,14 @@
-const { default: Axios } = require('axios');
+const {default : Axios} = require('axios');
 
 module.exports.dotaMeme = async () => {
-    try {
-        const res = await Axios.get(
-            'https://meme-api.herokuapp.com/gimme/Dota2Memes'
-        );
+  try {
+    const res =
+        await Axios.get('https://meme-api.herokuapp.com/gimme/Dota2Memes');
 
-        const imageArrLen = res.data.preview.length;
-        const imageUrl = res.data.preview[imageArrLen - 1];
-        return imageUrl;
-    } catch (error) {
-        console.log(error);
-    }
+    const imageArrLen = res.data.preview.length;
+    const imageUrl = res.data.preview[imageArrLen - 1];
+    return imageUrl;
+  } catch (error) {
+    console.log(error);
+  }
 };
